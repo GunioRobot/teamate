4a3b6d32ec940efe2ef86680afa18e6426170a38
Sasa Ranisavljevic <sasa.ranisavljevic@gmail.com>
2011-04-12 13:16:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vitruvius::Application.config.secret_token = '83289edf63677b0bad9285721a358108ec5312b5b5180b1c306e3c415fd1080395f1dc2b99aa19d50b048fed9a36654ecb34d9a514a6630c0620b46177d03bde'
