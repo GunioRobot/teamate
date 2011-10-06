c32d1237b3f851dcb7f1cbbf8e4e85b0c5dfa202
Stanko Tadic <stanko87@gmail.com>
2011-10-06 16:42:37 +0200

    Still working on static content/design

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
-
     // main menu fade
 	$('#menu').children().hover(function() {
 		$(this).siblings().stop().fadeTo(300,0.5);
-
 	}, function() {
 		$(this).siblings().stop().fadeTo(500,1);
 	});
@@ -21,3 +19,20 @@ $(document).ready(function(){
 
 });
 
+function showOutdatedBrowserDiv(){
+    var outdatedDiv; 
+    outdatedDiv = '<div class="alert-message block-message error" id="outdated" style="display:none">';
+    outdatedDiv += '<a href="javascript:void(0)" class="close" onclick="$(this).parent().slideUp(800)">×</a>';
+    outdatedDiv += '<strong>You\'re using outdated browser</strong>';
+    outdatedDiv += '<p>';
+    outdatedDiv += 'Please download one of the modern browsers.';
+    outdatedDiv += '</p>';
+    outdatedDiv += '<img src="http://paulirish.com/lovesyou/browser_logos-32.png" alt="Modern browsers"/>';
+    outdatedDiv += '<p>';
+    outdatedDiv += 'Modern browsers will improve your overall internet experience! Yay!';
+    outdatedDiv += '</p>';
+    outdatedDiv += '</div>';
+    
+    $('ul.breadcrumb').before(outdatedDiv);
+    $('#outdated').slideDown(800)
+}
